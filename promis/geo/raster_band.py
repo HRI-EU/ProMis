f13f8df95ce0e56e21fc404e3ea6101f5af91ba8
Felix Divo <felix.divo@cs.tu-darmstadt.de>
2024-05-08 17:56:09 +0000

    Imporove RasterBand plotting

--- promis/geo/raster_band.py
+++ promis/geo/raster_band.py
@@ -15,19 +15,24 @@ from itertools import product
 # Third Party
 from numpy import array, concatenate, linspace, meshgrid, ravel, vstack, zeros
 from pandas import DataFrame
+import matplotlib.pyplot as plt
+from matplotlib.transforms import Bbox
+from numpy import ndarray, array, sum, uint8, vstack, zeros
+from PIL import Image
+from sklearn.preprocessing import MinMaxScaler
 
 # ProMis
 from promis.geo import (
     CartesianCollection,
     CartesianLocation,
+    CartesianPolygon,
     PolarCollection,
     PolarLocation,
 )
 
 
-class RasterBand(ABC):
-
-    """A raster-band of spatially referenced data.
+class RasterBand:
+    """A Cartesian raster-band representing map data concerning a location type.
 
     Args:
         resolution: The number of horizontal and vertical pixels
@@ -37,7 +42,8 @@ class RasterBand(ABC):
 
     def __init__(
         self,
-        resolution: tuple[int, int],
+        resolution: tuple[int, int] | ndarray,  # or data
+        origin: PolarLocation,
         width: float,
         height: float,
     ):
@@ -45,29 +51,87 @@ class RasterBand(ABC):
         self.resolution = resolution
         self.width = width
         self.height = height
-        self.pixel_width = self.width / self.resolution[0]
-        self.pixel_height = self.height / self.resolution[1]
+        if isinstance(resolution, ndarray):
+            self.data = resolution
+        else:
+            self.data = zeros(resolution)
+
+        # Dimension of each pixel in meters
+        self.pixel_width = self.width / self.data.shape[0]
+        self.pixel_height = self.height / self.data.shape[1]
+
+        # Location of center in meters relative to top-left corner
         self.center_x = self.width / 2
         self.center_y = self.height / 2
 
     def append(self, location: CartesianLocation | PolarLocation, values: list[float]):
         raise Exception("RasterBand cannot be extended with locations!")
 
+    @classmethod
+    def from_map(
+        cls,
+        map: PolarMap | CartesianMap,
+        location_type: LocationType,
+        resolution: tuple[int, int],
+        normalize: bool = True,
+    ) -> "RasterBand":
+        """Takes a PolarMap or CartesianMap to initialize the raster band data.
 
-class CartesianRasterBand(RasterBand, CartesianCollection):
+        Args:
+            map: The map to read from
+            location_type: The location type to create a raster-band from
+            resolution: The resolution of the raster-band data
+            normalize: Whether to normalize the raster-band data to ``[0, 1]``
 
-    """A raster-band of Cartesian referenced data.
-
-    Args:
-        origin: The polar coordinates of this raster-band's center
-        resolution: The number of horizontal and vertical pixels
-        width: The width the raster band stretches over in meters
-        height: The height the raster band stretches over in meters
-        number_of_values: How many values are stored per location
     """
 
-    def __init__(
-        self,
+        # Attributes setup
+        map = map if isinstance(map, CartesianMap) else map.to_cartesian()
+
+        # Create and prepare figure for plotting
+        figure, axis = plt.subplots(figsize=(map.width, map.height), dpi=1)
+        axis.set_aspect("equal")
+        axis.set_axis_off()
+        axis.set_xlim([-map.width, map.width])
+        axis.set_ylim([-map.height, map.height])
+
+        # Plot all features with this type
+        # TODO: Only considers polygons right now
+        for feature in map.features:
+            if isinstance(feature, CartesianPolygon) and feature.location_type == location_type:
+                feature.plot(axis, facecolor="black")
+
+        # Create a bounding box with the actual map data
+        figure.canvas.draw()
+        bounding_box = Bbox([[-map.width / 2, -map.height / 2], [map.width / 2, map.height / 2]])
+        bounding_box = bounding_box.transformed(axis.transData).transformed(
+            figure.dpi_scale_trans.inverted()
+        )
+
+        # Get image from figure and check if it is empty
+        raster_band_image = cls._figure_to_image(figure, bounding_box)
+
+        # Clean up
+        plt.close(figure)
+
+        # Resize to specified resolution
+        raster_band_image = raster_band_image.resize(resolution)
+
+        # Convert to numpy and normalize from discrete [0, 255] to continuous [0, 1]
+        # Since we draw existing features in black on a white background, we invert colors
+        # Also drop two of the three sub-bands since all are equal
+        data = array(raster_band_image, dtype="float32")
+        data = data[:, :, 0]
+        if normalize:
+            data /= 255.0
+            data = 1 - data
+
+        return cls(data, map.origin, map.width, map.height)
+
+    @classmethod
+    def from_gaussian_mixture(
+        cls,
+        gaussian_mixture: GaussianMixture,
         origin: PolarLocation,
         resolution: tuple[int, int],
         width: float,
