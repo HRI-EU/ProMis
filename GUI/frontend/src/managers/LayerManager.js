c09687316e459be2a2a5a502311fa2a905b13ce2
Duc Hoang <hoangvanduccs@gmail.com>
2024-08-28 13:44:20 +0200

    add visualization program

---
+++ GUI/frontend/src/managers/LayerManager.js
@@ -0,0 +1,233 @@
+import Layer from "../models/Layer.js";
+import { C } from "./Core.js";
+import { RenderMode } from "./MapManager.js";
+
+class LayerManager {
+  constructor() {
+    this.layers = [];
+    this.hideAllLayers = false;
+  }
+
+  //Toggle all layers visibility
+  toggleAllLayers() {
+    this.hideAllLayers = !this.hideAllLayers;
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  //Import all layers from json file
+  importAllLayers(layers) {
+    for (let i = 0; i < layers.length; i++) {
+      layers[i].renderMode = RenderMode.HeatmapRect;
+    }
+    this.layers = layers;
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  /**
+   *
+   * @param {number[][3]} data data from the csv file
+   * @param {*} fileInfo file information to get name
+   */
+  importLayer(data, fileInfo) {
+    const uniqueId = Date.now();
+    const layer = Layer.parseLayer(uniqueId, data, 180.0, fileInfo.name, 5);
+    this.layers.push(layer);
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  /**
+   * Delete all previously added layers from the map and from the SidebarLeft
+   */
+  deleteAllLayers() {
+    this.layers = [];
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  //Delete layer with this layerId
+  deleteLayer(layerId) {
+    this.layers = this.layers.filter((layer) => layer.id !== layerId);
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  //Toggle layer visibility
+  changeLayerVisibility(layerId, visible) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    this.layers[pos].visible = visible;
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  //Toggle settings menu
+  changeLayerSettingsExpanded(layerId, expanded) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    if (expanded) {
+      this.layers[pos].colorMenuExpanded = false;
+    }
+    this.layers[pos].settingsMenuExpanded = expanded;
+    C().updateSidebarRight();
+  }
+
+  //Toggle color menu
+  changeLayerColorsExpanded(layerId, expanded) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    if (expanded) {
+      this.layers[pos].settingsMenuExpanded = false;
+    }
+    this.layers[pos].colorMenuExpanded = expanded;
+    C().updateSidebarRight();
+  }
+
+  //Change layer color
+  changeLayerColor(layerId, hue) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    this.layers[pos].hue = hue;
+    C().updateSidebarRight();
+    C().mapMan.updateLayerColor(this.layers[pos]);
+  }
+
+  //Change layer opacity
+  changeLayerOpacity(layerId, opacity) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    this.layers[pos].opacity = opacity;
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  //Change layer render mode
+  changeLayerRenderMode(layerId, renderMode) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    this.layers[pos].renderMode = renderMode;
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  //Change layer radius
+  changeLayerRadius(layerId, radius) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    this.layers[pos].radius = radius;
+    var markerDst = Layer.calcMarkerDst(this.layers[pos].markers, radius);
+    this.layers[pos].markerDstLat = markerDst[0];
+    this.layers[pos].markerDstLng = markerDst[1];
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  //Change layer value range
+  changeLayerValueRange(layerId, valueRange) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    this.layers[pos].valueRange = valueRange;
+    C().updateSidebarRight();
+    C().mapMan.refreshMap();
+  }
+
+  //If within bounds, switch layer with previous in array (moves layer in sidebar upwards)
+  moveLayerUp(layerId) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    if (pos > 0) {
+      console.log("up before ", this.layers);
+
+      // remove `from` item and store it
+      var f = this.layers.splice(pos, 1)[0];
+      // insert stored item into position `to`
+      this.layers.splice(pos - 1, 0, f);
+
+      console.log("up after ", this.layers);
+
+      //var lay = this.layers[pos];
+      //this.layers[pos] = this.layers[pos-1];
+      //this.layers[pos-1] = lay;
+      C().updateSidebarRight();
+      C().mapMan.refreshMap();
+    }
+  }
+
+  //If within bounds, switch layer with next in array (moves layer in sidebar downwards)
+  moveLayerDown(layerId) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    if (pos < this.layers.length) {
+      console.log("down before ", this.layers);
+
+      // remove `from` item and store it
+      var f = this.layers.splice(pos, 1)[0];
+      // insert stored item into position `to`
+      this.layers.splice(pos + 1, 0, f);
+
+      console.log("down after ", this.layers);
+
+      //var lay = this.layers[pos];
+      //this.layers[pos] = this.layers[pos+1];
+      //this.layers[pos+1] = lay;
+      C().updateSidebarRight();
+      C().mapMan.refreshMap();
+    }
+  }
+
+  //Moves map to center pos [lat, lng] of given layer
+  moveToLayerCenter(layerId) {
+    var pos = LayerManager.findLayerPos(this.layers, layerId);
+    var centerLatLng = Layer.calcCenterLatLng(
+      this.layers[pos].markersLatMinMax,
+      this.layers[pos].markersLngMinMax,
+    );
+    C().mapMan.moveTo(centerLatLng);
+    //console.log("centerLatLng is ", centerLatLng );
+  }
+
+  /**
+   * Get layer by id
+   * @param {number} layerId
+   * @returns {Layer | undefined} Layer object or undefined if not found
+   */
+  getLayerById(layerId) {
+    return LayerManager.findLayer(this.layers, layerId);
+  }
+
+  /**
+   * Export layer to GeoJSON
+   * @param {number} layerId
+   * @returns {object} GeoJSON object
+   */
+  exportGeoJSON(layerId) {
+    return LayerManager.findLayer(this.layers, layerId).markerLayer.toGeoJSON();
+  }
+
+  /**
+   * Export all layers to GeoJSON including markers
+   * @returns {object} GeoJSON object
+   */
+  exportAllGeoJSON() {
+    const dynamicFeatureGroup = C().mapMan.dynamicFeatureGroup;
+    if (
+      this.layers.length === 0 &&
+      dynamicFeatureGroup.getLayers().length === 0
+    ) {
+      return null;
+    }
+    var geoJSON = {
+      type: "FeatureCollection",
+      features: [],
+    };
+    this.layers.forEach((layer) => {
+      geoJSON.features.push(...layer.markerLayer.toGeoJSON().features);
+    });
+    geoJSON.features.push(...dynamicFeatureGroup.toGeoJSON().features);
+    return geoJSON;
+  }
+
+  //Takes layers array and layerId, returns layer or undefined
+  static findLayer(layers, layerId) {
+    return layers[this.findLayerPos(layers, layerId)];
+  }
+
+  //Takes layers array and layerId, returns layer pos in array or -1 if not found
+  static findLayerPos(layers, layerId) {
+    return layers.findIndex((layer) => layer.id === layerId);
+  }
+}
+
+export default LayerManager;
---
+++ GUI/frontend/src/models/Layer.js
@@ -0,0 +1,171 @@
+import { RenderMode } from "../managers/MapManager";
+
+export default class Layer {
+  /**
+   * Check if the longitude is not NaN and is in the range [-180, 180]
+   * @param {*} longitude
+   * @returns {boolean} A boolean value, if the longitude is valid or not
+   */
+  static validLongitude(longitude) {
+    return !isNaN(longitude) && longitude >= -180 && longitude <= 180;
+  }
+
+  /**
+   * Check if the latitude is not NaN and is in the range [-90, 90]
+   * @param {*} latitude
+   * @returns {boolean} A boolean value, if the latitude is valid or not
+   */
+  static validLatitude(latitude) {
+    return !isNaN(latitude) && latitude >= -90 && latitude <= 90;
+  }
+
+  /**
+   * Check if the probability is not NaN and is in the range [0, 1]
+   * @param {*} probability
+   * @returns {boolean} A boolean value, if the probability is valid or not
+   */
+  static validProbability(probability) {
+    return !isNaN(probability) && probability >= 0 && probability <= 1;
+  }
+
+  constructor(
+    id,
+    markers,
+    hue,
+    name,
+    radius,
+    markerDstLat,
+    markerDstLng,
+    markersValMinMax,
+    markersLatMinMax,
+    markersLngMinMax,
+  ) {
+    this.id = id;
+    this.name = name || ""; // String type field
+    this.markers = markers || []; // Array type field
+
+    this.visible = true;
+    this.settingsMenuExpanded = false;
+    this.colorMenuExpanded = false;
+
+    this.hue = typeof hue === "number" ? hue : 0.0; // Float type field
+    this.opacity = 0.3;
+
+    this.renderMode = RenderMode.HeatmapRect;
+    this.radius = radius || 1.0; // Float type field
+    this.valueRange = markersValMinMax || [0, 1];
+    this.markersValMinMax = markersValMinMax || [0, 1]; // [min, max] probability of markers array
+
+    this.markersLatMinMax = markersLatMinMax || [0, 0]; // [min, max] lat of markers array
+    this.markersLngMinMax = markersLngMinMax || [0, 0]; // [min, max] lng of markers array
+
+    this.markerDstLat = markerDstLat || 0.00005;
+    this.markerDstLng = markerDstLng || 0.00005;
+
+    this.markerLayer = null;
+    this.leafletOverlays = [];
+
+    this.colorMenuLatestSelection = null;
+    this.colorMenuHueSlider = 0;
+
+    this.isEnable = true;
+  }
+
+  /**
+   * Create a new layer from the given data
+   *
+   * @param {number} id
+   * @param {number[][3]} data row of [lat, lng, probability] array
+   * @param {number} hue color hur from 0 to 360
+   * @param {string} name name of the layer
+   * @param {number} radius radius of the marker
+   * @returns
+   */
+  static parseLayer(id, data, hue, name, radius) {
+    const markers = [];
+    const markersValMinMax = [0, 0];
+    const markersLatMinMax = [90, -90]; //Every value will be smaller than 90 and bigger than -90
+    const markersLngMinMax = [180, -180];
+    data.forEach((row) => {
+      if (
+        Layer.validLatitude(parseFloat(row[0])) &&
+        Layer.validLongitude(parseFloat(row[1]))
+      ) {
+        markers.push({
+          position: [parseFloat(row[0]), parseFloat(row[1])],
+          probability: parseFloat(row[2]),
+          radius: radius
+        });
+        //Find min max values of markers
+        var val = parseFloat(row[2]);
+        if (val < markersValMinMax[0]) {
+          //New val min found
+          markersValMinMax[0] = val;
+        } else if (val > markersValMinMax[1]) {
+          //New val max found
+          markersValMinMax[1] = val;
+        }
+        var lat = parseFloat(row[0]);
+        if (lat < markersLatMinMax[0]) {
+          //New lat min found
+          markersLatMinMax[0] = lat;
+        } else if (lat > markersLatMinMax[1]) {
+          //New lat max found
+          markersLatMinMax[1] = lat;
+        }
+        var lng = parseFloat(row[1]);
+        if (lng < markersLngMinMax[0]) {
+          //New lng min found
+          markersLngMinMax[0] = lng;
+        } else if (lng > markersLngMinMax[1]) {
+          //New lng max found
+          markersLngMinMax[1] = lng;
+        }
+      }
+    });
+
+    console.log("latMinMax ", markersLatMinMax);
+    console.log("lngMinMax ", markersLngMinMax);
+
+    const markerDst = Layer.calcMarkerDst(markers, radius);
+    return new Layer(
+      id,
+      markers,
+      hue,
+      name,
+      radius,
+      markerDst[0],
+      markerDst[1],
+      markersValMinMax,
+      markersLatMinMax,
+      markersLngMinMax,
+    );
+  }
+
+  /**
+   * Calculate the [lat, lng] distance based on the radius of rendered points and the first marker position
+   * Distance in lat lng is not linear to meters and ratio of lat and lng changes with the position
+   * Returns [0.00003, 0.00003] if no markers given
+   * @param {*} markers Marker array
+   * @param {*} radius Radius in meters
+   * @returns {[int, int]} [x,y] array of lat,lng distances
+   */
+  static calcMarkerDst(markers, radius) {
+    if (markers != null && markers.length > 0) {
+      const latSample = markers[0].position[0];
+      const markerDstLat = radius / 111111;
+      const markerDstLng =
+        radius / (111111 * Math.cos(latSample * (Math.PI / 180)));
+      return [markerDstLat, markerDstLng];
+    } else {
+      return [0.00003, 0.00003];
+    }
+  }
+
+  // Calculate center position [lat, lng] of given latMinMax and lngMinMax
+  static calcCenterLatLng(latMinMax, lngMinMax) {
+    var latCenter = latMinMax[0] + 0.5 * (latMinMax[1] - latMinMax[0]);
+    var lngCenter = lngMinMax[0] + 0.5 * (lngMinMax[1] - lngMinMax[0]);
+    return [latCenter, lngCenter];
+  }
+}
