933ad87d68038eb2af4776ef1718c98eda1f605a
Simon Kohaut <simon.kohaut@cs.tu-darmstadt.de>
2025-05-13 11:32:03 +0200

    Some intermediate experiment code with velocity and heading

--- promis/coco.py
+++ promis/coco.py
@@ -15,7 +15,7 @@ from pickle import dump, load
 
 # Third Party
 import torch
-from nflows.distributions.normal import ConditionalDiagonalNormal
+from nflows.distributions.normal import ConditionalDiagonalNormal, StandardNormal
 from nflows.flows.base import Flow
 from nflows.transforms.autoregressive import MaskedAffineAutoregressiveTransform
 from nflows.transforms.base import CompositeTransform
@@ -51,7 +51,7 @@ class DoubtDensity:
         ])
 
         # Need to ensure even number of features for nflows library
-        if self.number_of_context_features % 2 == 1:
+        if self.number_of_context_features > 2 and self.number_of_context_features % 2 == 1:
             self.number_of_context_features += 1
 
         # Flow setup
@@ -66,7 +66,9 @@ class DoubtDensity:
 
         self.flow = Flow(
             CompositeTransform(transforms), 
-            ConditionalDiagonalNormal(shape=[self.number_of_states])
+            StandardNormal(
+                shape=[self.number_of_states]
+            )
         )
 
     @staticmethod
@@ -78,35 +80,9 @@ class DoubtDensity:
         with open(path, "wb") as file:
             dump(self, file)
 
-    def sample(self, size: int, doubt_space: dict[str, dict]):
-        return self.flow.sample(size, context=self.context_encoder(doubt_space))
-
-    def log_prob(self, states: NDArray, doubt_space: dict[str, dict]):
-        return self.flow.log_prob(states, context=self.context_encoder(doubt_space))
-
-    def prob(self, states: NDArray, doubt_space: dict[str, dict]):
-        return self.log_prob(states, doubt_space).exp()
-
-    def fit(self, samples: NDArray, doubt_space: dict[str, dict], number_of_epochs: int, batch_size: int) -> NDArray:
-        # Setup optimizer
-        optimizer = torch.optim.Adam(self.flow.parameters())
-        dataloader = torch.utils.data.DataLoader(torch.hstack([samples, self.context_encoder(doubt_space)]), batch_size=batch_size, shuffle=True)
-
-        # Train on given samples and doubt features
-        losses = []
-        for epoch in tqdm(range(number_of_epochs)):
-            for batch in dataloader:
-                optimizer.zero_grad()
-                loss = -self.flow.log_prob(batch[:, 0:self.number_of_states], context=batch[:, self.number_of_states:]).mean()
-                loss.backward()
-                optimizer.step()
-                losses.append(loss.detach().numpy())
-
-        return losses
-
-    def context_encoder(self, doubt_space: dict[str, dict]):
+    def doubt_space_to_tensor(self, doubt_space: dict[str, dict]):
         # One hot encode categorical features
-        # Standard scale continuous features
+        # TODO: Apply chosen normalization to continuous features
         encoded_features = []
         for feature in doubt_space.keys():
             if doubt_space[feature]['type'] == 'categorical':
@@ -125,6 +101,33 @@ class DoubtDensity:
 
         return context
 
+    def sample(self, size: int, doubt_space: dict[str, dict]):
+        return self.flow.sample(size, context=self.doubt_space_to_tensor(doubt_space))
+
+    def log_prob(self, states: NDArray, doubt_space: dict[str, dict]):
+        return self.flow.log_prob(states, context=self.doubt_space_to_tensor(doubt_space))
+
+    def prob(self, states: NDArray, doubt_space: dict[str, dict]):
+        return self.log_prob(states, doubt_space).exp()
+
+    def fit(self, samples: NDArray, doubt_space: dict[str, dict], number_of_epochs: int, batch_size: int) -> NDArray:
+        # Setup optimizer
+        optimizer = torch.optim.Adam(self.flow.parameters())
+        dataloader = torch.utils.data.DataLoader(torch.hstack([samples, self.doubt_space_to_tensor(doubt_space)]), batch_size=batch_size, shuffle=True)
+
+        # Train on given samples and doubt features
+        losses = []
+        for epoch in tqdm(range(number_of_epochs), desc="Learning Doubt Density", unit="epoch"):
+            for batch in dataloader:
+                optimizer.zero_grad()
+                loss = -self.flow.log_prob(batch[:, :self.number_of_states], context=batch[:, self.number_of_states:]).mean()
+                loss.backward()
+                optimizer.step()
+                losses.append(loss.detach().numpy())
+
+        return losses
+
+
 
 class ConstitutionalController:
 
@@ -133,10 +136,11 @@ class ConstitutionalController:
         landscape: Collection,
         doubt_density: Callable[[int], NDArray],
         doubt_space: dict[str, dict],
-        number_of_samples: int
+        number_of_samples: int,
+        scaler=1000.0
     ) -> Collection:
         interpolator = landscape.get_interpolator("hybrid")
-        samples = doubt_density.sample(number_of_samples, doubt_space)[0]
+        samples = scaler * doubt_density.sample(number_of_samples, doubt_space)[0]
 
         doubtful_landscape = deepcopy(landscape)
         doubtful_landscape.data['v0'] = [
