322ac1c44a9f35e3d66e64f1c7b3c1ca7473e2f5
Duc Hoang <hoangvanduccs@gmail.com>
2025-10-04 15:24:16 +0200

    fix frontent path in script

--- scripts/build_gui_dev.sh
+++ scripts/build_gui_dev.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Navigate to the frontend folder
-cd "$SCRIPT_DIR/gui/frontend" || exit 1
+cd "$SCRIPT_DIR/../gui/frontend" || exit 1
 
 # Install dependencies and build the frontend
 npm install
