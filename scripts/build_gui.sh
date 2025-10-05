1686722602fe5395a8694e928cfaba96996880a2
Duc Hoang <hoangvanduccs@gmail.com>
2025-10-05 10:29:42 +0200

    fix build path in script

--- scripts/build_gui.sh
+++ scripts/build_gui.sh
@@ -10,10 +10,10 @@ npm install
 npm run build
 
 # Define the build output and target directory
-BUILD_DIR="$SCRIPT_DIR/gui/frontend/build"
-DATA_DIR="$SCRIPT_DIR/resources/data"
-TARGET_DIR="$SCRIPT_DIR/promis/gui/frontend"
-DATA_TARGER_DIR="$SCRIPT_DIR/promis/gui/data"
+BUILD_DIR="$SCRIPT_DIR/../gui/frontend/build"
+DATA_DIR="$SCRIPT_DIR/../resources/data"
+TARGET_DIR="$SCRIPT_DIR/../promis/gui/frontend"
+DATA_TARGER_DIR="$SCRIPT_DIR/../promis/gui/data"
 
 # Create the target directory if it doesn't exist
 mkdir -p "$TARGET_DIR"
